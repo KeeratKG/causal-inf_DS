af675bd9b8e0a9f83ff2f39f552fe8905c3b20f2
KeeratKG <keerat.kaur.guliani@gmail.com>
2021-02-01 17:12:26 +0530

    Add graph.

--- causalML/causalForest.r
+++ causalML/causalForest.r
@@ -157,3 +157,4 @@ data.frame("est"=pred,"Registration.Source"=dat$Registration.Source) %>%
 +   theme(legend.position="none") -->
 
 ![alt text](https://github.com/KeeratKG/causal-inf_DS/blob/main/media/causaML.png)
+/home/keerat/github/causal-inf_DS/media/causaML.png
